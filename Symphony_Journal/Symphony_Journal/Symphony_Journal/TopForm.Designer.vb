f5c21a7a11b0677d978c1192538cd7d4ca4d5500
hjnb <32120439+hjnb@users.noreply.github.com>
2019-01-18 17:07:37 +0900

    Add files via upload

---
+++ Symphony_Journal/Symphony_Journal/Symphony_Journal/TopForm.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TopForm
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.topPicture = New System.Windows.Forms.PictureBox()
+        Me.rbtnPreview = New System.Windows.Forms.RadioButton()
+        Me.rbtnPrintout = New System.Windows.Forms.RadioButton()
+        CType(Me.topPicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'topPicture
+        '
+        Me.topPicture.Location = New System.Drawing.Point(589, 59)
+        Me.topPicture.Name = "topPicture"
+        Me.topPicture.Size = New System.Drawing.Size(147, 142)
+        Me.topPicture.TabIndex = 0
+        Me.topPicture.TabStop = False
+        '
+        'rbtnPreview
+        '
+        Me.rbtnPreview.AutoSize = True
+        Me.rbtnPreview.Location = New System.Drawing.Point(586, 223)
+        Me.rbtnPreview.Name = "rbtnPreview"
+        Me.rbtnPreview.Size = New System.Drawing.Size(67, 16)
+        Me.rbtnPreview.TabIndex = 1
+        Me.rbtnPreview.TabStop = True
+        Me.rbtnPreview.Text = "プレビュー"
+        Me.rbtnPreview.UseVisualStyleBackColor = True
+        '
+        'rbtnPrintout
+        '
+        Me.rbtnPrintout.AutoSize = True
+        Me.rbtnPrintout.Location = New System.Drawing.Point(675, 223)
+        Me.rbtnPrintout.Name = "rbtnPrintout"
+        Me.rbtnPrintout.Size = New System.Drawing.Size(47, 16)
+        Me.rbtnPrintout.TabIndex = 2
+        Me.rbtnPrintout.TabStop = True
+        Me.rbtnPrintout.Text = "印刷"
+        Me.rbtnPrintout.UseVisualStyleBackColor = True
+        '
+        'TopForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(958, 691)
+        Me.Controls.Add(Me.rbtnPrintout)
+        Me.Controls.Add(Me.rbtnPreview)
+        Me.Controls.Add(Me.topPicture)
+        Me.Name = "TopForm"
+        Me.Text = "介護日誌"
+        CType(Me.topPicture, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents topPicture As System.Windows.Forms.PictureBox
+    Friend WithEvents rbtnPreview As System.Windows.Forms.RadioButton
+    Friend WithEvents rbtnPrintout As System.Windows.Forms.RadioButton
+
+End Class
---
+++ Symphony_Journal/Symphony_Journal/Symphony_Journal/TopForm.vb
@@ -0,0 +1,77 @@
+﻿Public Class TopForm
+    'データベースのパス
+    Public dbFilePath As String = My.Application.Info.DirectoryPath & "\Journal.mdb"
+    Public DB_Journal As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & dbFilePath
+
+    'エクセルのパス
+    Public excelFilePass As String = My.Application.Info.DirectoryPath & "\Journal.xls"
+
+    '.iniファイルのパス
+    Public iniFilePath As String = My.Application.Info.DirectoryPath & "\Journal1.ini"
+
+    '画像パス
+    Public imageFilePath As String = My.Application.Info.DirectoryPath & "\Div1.wmf"
+
+    Private Sub TopForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'データベース、エクセル、構成ファイルの存在チェック
+        If Not System.IO.File.Exists(dbFilePath) Then
+            MsgBox("データベースファイルが存在しません。ファイルを配置して下さい。")
+            Me.Close()
+            Exit Sub
+        End If
+
+        If Not System.IO.File.Exists(excelFilePass) Then
+            MsgBox("エクセルファイルが存在しません。ファイルを配置して下さい。")
+            Me.Close()
+            Exit Sub
+        End If
+
+        If Not System.IO.File.Exists(iniFilePath) Then
+            MsgBox("構成ファイルが存在しません。ファイルを配置して下さい。")
+            Me.Close()
+            Exit Sub
+        End If
+
+        If Not System.IO.File.Exists(imageFilePath) Then
+            MsgBox("画像ファイルが存在しません。ファイルを配置して下さい。")
+            Me.Close()
+            Exit Sub
+        End If
+
+        '画面サイズ等
+        Me.WindowState = FormWindowState.Maximized
+        Me.MinimizeBox = False
+        Me.MaximizeBox = False
+
+        '画像の配置処理
+        topPicture.ImageLocation = imageFilePath
+
+        '印刷ラジオボタンの初期設定
+        initPrintState()
+    End Sub
+
+    Private Sub initPrintState()
+        Dim state As String = Util.getIniString("System", "Printer", iniFilePath)
+        If state = "Y" Then
+            rbtnPrintout.Checked = True
+        Else
+            rbtnPreview.Checked = True
+        End If
+    End Sub
+
+    Private Sub rbtnPreview_CheckedChanged(sender As Object, e As System.EventArgs) Handles rbtnPreview.CheckedChanged
+        If rbtnPreview.Checked = True Then
+            Util.putIniString("System", "Printer", "N", iniFilePath)
+        End If
+    End Sub
+
+    Private Sub rbtnPrint_CheckedChanged(sender As Object, e As System.EventArgs) Handles rbtnPrintout.CheckedChanged
+        If rbtnPrintout.Checked = True Then
+            Util.putIniString("System", "Printer", "Y", iniFilePath)
+        End If
+    End Sub
+
+    Private Sub topPicture_Click(sender As System.Object, e As System.EventArgs) Handles topPicture.Click
+        Me.Close()
+    End Sub
+End Class
