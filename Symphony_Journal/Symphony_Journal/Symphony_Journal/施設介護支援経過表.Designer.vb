279a347f08a57f2e2af420596b697ed5a1c96531
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2019-02-18 17:11:45 +0900

    湿雪介護支援：デザイン作成など

--- Symphony_Journal/Symphony_Journal/Symphony_Journal/施設介護支援経過表.Designer.vb
+++ Symphony_Journal/Symphony_Journal/Symphony_Journal/施設介護支援経過表.Designer.vb
@@ -22,8 +22,264 @@ Partial Class 施設介護支援経過表
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.namLabel = New System.Windows.Forms.Label()
+        Me.YmdBox = New ymdBox.ymdBox()
+        Me.btnRegist = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnPrint = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tantoBox = New System.Windows.Forms.ComboBox()
+        Me.dgvSkei = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.residentListBox = New System.Windows.Forms.ListBox()
+        Me.unitListBox = New System.Windows.Forms.ListBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.historyListBox = New System.Windows.Forms.ListBox()
+        Me.btnRowInsert = New System.Windows.Forms.Button()
+        Me.btnRowDelete = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.writerLabel = New System.Windows.Forms.Label()
+        Me.writerListBox = New System.Windows.Forms.ListBox()
+        CType(Me.dgvSkei, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'namLabel
+        '
+        Me.namLabel.AutoSize = True
+        Me.namLabel.Font = New System.Drawing.Font("MS UI Gothic", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.namLabel.ForeColor = System.Drawing.Color.Blue
+        Me.namLabel.Location = New System.Drawing.Point(39, 29)
+        Me.namLabel.Name = "namLabel"
+        Me.namLabel.Size = New System.Drawing.Size(85, 19)
+        Me.namLabel.TabIndex = 0
+        Me.namLabel.Text = "蛯子　トヨ"
+        '
+        'YmdBox
+        '
+        Me.YmdBox.boxType = 8
+        Me.YmdBox.DateText = ""
+        Me.YmdBox.EraLabelText = "H31"
+        Me.YmdBox.EraText = ""
+        Me.YmdBox.Location = New System.Drawing.Point(171, 15)
+        Me.YmdBox.MonthLabelText = "02"
+        Me.YmdBox.MonthText = ""
+        Me.YmdBox.Name = "YmdBox"
+        Me.YmdBox.Size = New System.Drawing.Size(174, 46)
+        Me.YmdBox.TabIndex = 1
+        '
+        'btnRegist
+        '
+        Me.btnRegist.Location = New System.Drawing.Point(380, 18)
+        Me.btnRegist.Name = "btnRegist"
+        Me.btnRegist.Size = New System.Drawing.Size(65, 36)
+        Me.btnRegist.TabIndex = 2
+        Me.btnRegist.Text = "登録"
+        Me.btnRegist.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(444, 18)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(65, 36)
+        Me.btnDelete.TabIndex = 3
+        Me.btnDelete.Text = "削除"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnPrint
+        '
+        Me.btnPrint.Location = New System.Drawing.Point(508, 18)
+        Me.btnPrint.Name = "btnPrint"
+        Me.btnPrint.Size = New System.Drawing.Size(65, 36)
+        Me.btnPrint.TabIndex = 4
+        Me.btnPrint.Text = "印刷"
+        Me.btnPrint.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(615, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(116, 11)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "施設サービス計画作成者"
+        '
+        'tantoBox
+        '
+        Me.tantoBox.FormattingEnabled = True
+        Me.tantoBox.Location = New System.Drawing.Point(615, 37)
+        Me.tantoBox.Name = "tantoBox"
+        Me.tantoBox.Size = New System.Drawing.Size(114, 20)
+        Me.tantoBox.TabIndex = 6
+        '
+        'dgvSkei
+        '
+        Me.dgvSkei.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvSkei.Location = New System.Drawing.Point(171, 73)
+        Me.dgvSkei.Name = "dgvSkei"
+        Me.dgvSkei.RowTemplate.Height = 21
+        Me.dgvSkei.Size = New System.Drawing.Size(628, 633)
+        Me.dgvSkei.TabIndex = 7
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.residentListBox)
+        Me.GroupBox1.Controls.Add(Me.unitListBox)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 67)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(140, 300)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "入居者選択"
+        '
+        'residentListBox
+        '
+        Me.residentListBox.BackColor = System.Drawing.SystemColors.Control
+        Me.residentListBox.FormattingEnabled = True
+        Me.residentListBox.ItemHeight = 12
+        Me.residentListBox.Location = New System.Drawing.Point(8, 91)
+        Me.residentListBox.Name = "residentListBox"
+        Me.residentListBox.Size = New System.Drawing.Size(123, 196)
+        Me.residentListBox.TabIndex = 1
+        '
+        'unitListBox
+        '
+        Me.unitListBox.FormattingEnabled = True
+        Me.unitListBox.ItemHeight = 12
+        Me.unitListBox.Location = New System.Drawing.Point(17, 26)
+        Me.unitListBox.Name = "unitListBox"
+        Me.unitListBox.Size = New System.Drawing.Size(50, 52)
+        Me.unitListBox.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.historyListBox)
+        Me.GroupBox2.Location = New System.Drawing.Point(14, 376)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(140, 318)
+        Me.GroupBox2.TabIndex = 9
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "経過履歴"
+        '
+        'historyListBox
+        '
+        Me.historyListBox.BackColor = System.Drawing.SystemColors.Control
+        Me.historyListBox.Font = New System.Drawing.Font("MS UI Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.historyListBox.FormattingEnabled = True
+        Me.historyListBox.ItemHeight = 15
+        Me.historyListBox.Location = New System.Drawing.Point(8, 18)
+        Me.historyListBox.Name = "historyListBox"
+        Me.historyListBox.Size = New System.Drawing.Size(125, 289)
+        Me.historyListBox.TabIndex = 0
+        '
+        'btnRowInsert
+        '
+        Me.btnRowInsert.Location = New System.Drawing.Point(825, 93)
+        Me.btnRowInsert.Name = "btnRowInsert"
+        Me.btnRowInsert.Size = New System.Drawing.Size(64, 29)
+        Me.btnRowInsert.TabIndex = 10
+        Me.btnRowInsert.Text = "行挿入"
+        Me.btnRowInsert.UseVisualStyleBackColor = True
+        '
+        'btnRowDelete
+        '
+        Me.btnRowDelete.Location = New System.Drawing.Point(825, 121)
+        Me.btnRowDelete.Name = "btnRowDelete"
+        Me.btnRowDelete.Size = New System.Drawing.Size(64, 29)
+        Me.btnRowDelete.TabIndex = 11
+        Me.btnRowDelete.Text = "行削除"
+        Me.btnRowDelete.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(825, 149)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(64, 29)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "クリア"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.writerLabel)
+        Me.GroupBox3.Location = New System.Drawing.Point(827, 285)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(108, 44)
+        Me.GroupBox3.TabIndex = 13
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "記載者"
+        '
+        'writerLabel
+        '
+        Me.writerLabel.AutoSize = True
+        Me.writerLabel.ForeColor = System.Drawing.Color.Blue
+        Me.writerLabel.Location = New System.Drawing.Point(17, 21)
+        Me.writerLabel.Name = "writerLabel"
+        Me.writerLabel.Size = New System.Drawing.Size(0, 12)
+        Me.writerLabel.TabIndex = 0
+        '
+        'writerListBox
+        '
+        Me.writerListBox.BackColor = System.Drawing.SystemColors.Control
+        Me.writerListBox.FormattingEnabled = True
+        Me.writerListBox.ItemHeight = 12
+        Me.writerListBox.Location = New System.Drawing.Point(825, 342)
+        Me.writerListBox.Name = "writerListBox"
+        Me.writerListBox.Size = New System.Drawing.Size(117, 352)
+        Me.writerListBox.TabIndex = 14
+        '
+        '施設介護支援経過表
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(963, 722)
+        Me.Controls.Add(Me.writerListBox)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnRowDelete)
+        Me.Controls.Add(Me.btnRowInsert)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.dgvSkei)
+        Me.Controls.Add(Me.tantoBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnPrint)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnRegist)
+        Me.Controls.Add(Me.YmdBox)
+        Me.Controls.Add(Me.namLabel)
+        Me.Name = "施設介護支援経過表"
         Me.Text = "施設介護支援経過表"
+        CType(Me.dgvSkei, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents namLabel As System.Windows.Forms.Label
+    Friend WithEvents YmdBox As ymdBox.ymdBox
+    Friend WithEvents btnRegist As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnPrint As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents tantoBox As System.Windows.Forms.ComboBox
+    Friend WithEvents dgvSkei As System.Windows.Forms.DataGridView
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents residentListBox As System.Windows.Forms.ListBox
+    Friend WithEvents unitListBox As System.Windows.Forms.ListBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents historyListBox As System.Windows.Forms.ListBox
+    Friend WithEvents btnRowInsert As System.Windows.Forms.Button
+    Friend WithEvents btnRowDelete As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents writerLabel As System.Windows.Forms.Label
+    Friend WithEvents writerListBox As System.Windows.Forms.ListBox
 End Class
--- Symphony_Journal/Symphony_Journal/Symphony_Journal/施設介護支援経過表.vb
+++ Symphony_Journal/Symphony_Journal/Symphony_Journal/施設介護支援経過表.vb
@@ -1,3 +1,188 @@
 ﻿Public Class 施設介護支援経過表
 
+    'ユニット
+    Private unitArray() As String = {"星", "森", "空", "月", "花", "海"}
+
+    ''' <summary>
+    ''' loadイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub 施設介護支援経過表_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.WindowState = FormWindowState.Maximized
+        Me.MaximizeBox = False
+
+        'dgv初期設定
+        initDgvSkei()
+
+        'ユニットをセット
+        unitListBox.Items.AddRange(unitArray)
+
+        '記載者リスト初期設定
+        initWriterList()
+
+        '現在日付をセット
+        YmdBox.setADStr(Today.ToString("yyyy/MM/dd"))
+    End Sub
+
+    Private Sub initDgvSkei()
+        Util.EnableDoubleBuffering(dgvSkei)
+
+        With dgvSkei
+            .AllowUserToAddRows = False '行追加禁止
+            .AllowUserToResizeColumns = False '列の幅をユーザーが変更できないようにする
+            .AllowUserToResizeRows = False '行の高さをユーザーが変更できないようにする
+            .AllowUserToDeleteRows = False '行削除禁止
+            .BorderStyle = BorderStyle.None
+            .MultiSelect = False
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect 'クリック時に行選択
+            .RowHeadersVisible = False
+            .RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing
+            .ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+            .ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .ColumnHeadersHeight = 18
+            .RowTemplate.Height = 13
+            .BackgroundColor = Color.FromKnownColor(KnownColor.Control)
+            .DefaultCellStyle.SelectionBackColor = Color.White
+            .DefaultCellStyle.SelectionForeColor = Color.Black
+            .ShowCellToolTips = False
+            .EnableHeadersVisualStyles = False
+            .ImeMode = Windows.Forms.ImeMode.Hiragana
+        End With
+
+        '空行追加等
+        Dim dt As New DataTable()
+        dt.Columns.Add("Text", Type.GetType("System.String"))
+        For i = 0 To 46
+            Dim row As DataRow = dt.NewRow()
+            row(0) = ""
+            dt.Rows.Add(row)
+        Next
+        dgvSkei.DataSource = dt
+
+        '幅設定等
+        With dgvSkei
+            With .Columns("Text")
+                .Width = 610
+                .HeaderText = "内　　容"
+            End With
+        End With
+    End Sub
+
+    ''' <summary>
+    ''' 記入者リストボックス初期設定
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub initWriterList()
+        Dim cnn As New ADODB.Connection
+        Dim rs As New ADODB.Recordset
+        Dim sql As String
+
+        'workの勤務表から当月の勤務者（パート除く）の名前取得
+        Dim ym As String = Today.ToString("yyyy/MM")
+        cnn.Open(TopForm.DB_Work)
+        sql = "select Nam from KinD where Ym='" & ym & "' And Rdr<>'' order by Seq2, Seq"
+        rs.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockReadOnly)
+        While Not rs.EOF
+            writerListBox.Items.Add(Util.checkDBNullValue(rs.Fields("Nam").Value))
+            rs.MoveNext()
+        End While
+        rs.Close()
+        cnn.Close()
+
+        'EtcMから名前取得
+        cnn.Open(TopForm.DB_Journal)
+        sql = "select Nam from EtcM order by Num"
+        rs.Open(sql, cnn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockReadOnly)
+        While Not rs.EOF
+            writerListBox.Items.Add(Util.checkDBNullValue(rs.Fields("Nam").Value))
+            rs.MoveNext()
+        End While
+        rs.Close()
+        cnn.Close()
+    End Sub
+
+    ''' <summary>
+    ''' 記入者リストボックス値変更イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub writerListBox_SelectedValueChanged(sender As Object, e As System.EventArgs) Handles writerListBox.SelectedValueChanged
+        Dim nam As String = writerListBox.SelectedItem
+        If nam <> "" Then
+            writerLabel.Text = nam
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 対象ユニットの入居者リストを取得
+    ''' </summary>
+    ''' <param name="unitName"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function getResidentList(unitName As String) As List(Of String)
+        Dim resultList As New List(Of String) '取得結果リスト
+        '対象のユニットの入居者リスト作成
+        Dim cn As New ADODB.Connection()
+        cn.Open(TopForm.DB_Journal)
+        Dim sql As String = "select Nam from UsrM where Dsp=1 And Unt='" & unitName & "' order by Kana"
+        Dim rs As New ADODB.Recordset
+        rs.Open(sql, cn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockOptimistic)
+        While Not rs.EOF
+            resultList.Add(Util.checkDBNullValue(rs.Fields("Nam").Value))
+            rs.MoveNext()
+        End While
+        Return resultList
+    End Function
+
+    ''' <summary>
+    ''' 対象の入居者の経過履歴リスト取得
+    ''' </summary>
+    ''' <param name="residentName"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function getHistoryList(residentName As String) As List(Of String)
+        Dim resultList As New List(Of String) '取得結果リスト
+        '対象の入居者の経過履歴リスト作成
+        Dim cn As New ADODB.Connection()
+        cn.Open(TopForm.DB_Journal)
+        Dim sql As String = "select distinct Ymd from Skei where Div=" & TopForm.DIV & " And Nam='" & residentName & "' order by Ymd Desc"
+        Dim rs As New ADODB.Recordset
+        rs.Open(sql, cn, ADODB.CursorTypeEnum.adOpenForwardOnly, ADODB.LockTypeEnum.adLockOptimistic)
+        While Not rs.EOF
+            Dim wareki As String = Util.convADStrToWarekiStr(Util.checkDBNullValue(rs.Fields("Ymd").Value))
+            resultList.Add(wareki)
+            rs.MoveNext()
+        End While
+        Return resultList
+    End Function
+
+    ''' <summary>
+    ''' ユニットリスト値変更イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub unitListBox_SelectedValueChanged(sender As Object, e As System.EventArgs) Handles unitListBox.SelectedValueChanged
+        Dim selectedUnitName As String = unitListBox.Text
+        residentListBox.Items.Clear()
+        residentListBox.Items.AddRange(getResidentList(selectedUnitName).ToArray())
+        historyListBox.Items.Clear()
+    End Sub
+
+    ''' <summary>
+    ''' 入居者リスト値変更イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub residentListBox_SelectedValueChanged(sender As Object, e As System.EventArgs) Handles residentListBox.SelectedValueChanged
+        Dim nam As String = residentListBox.Text
+        If nam <> "" Then
+            historyListBox.Items.Clear()
+            historyListBox.Items.AddRange(getHistoryList(nam).ToArray())
+        End If
+    End Sub
 End Class
