e362606514eb929ab71f7928839adc41ff6fe238
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2020-07-20 16:23:16 +0900

    ユニット日誌：西暦対応

--- Symphony_Journal/Symphony_Journal/Symphony_Journal/ユニット日誌.vb
+++ Symphony_Journal/Symphony_Journal/Symphony_Journal/ユニット日誌.vb
@@ -78,7 +78,7 @@ Public Class ユニット日誌
         unitListBox.SelectedIndex = 0
 
         '日付ボックス初期選択
-        YmdBox.setFocus(6)
+        YmdBox.setFocus(7)
 
         '日付ボックス上下ボタン長押し不可
         YmdBox.canHoldDownButton = False
@@ -679,13 +679,12 @@ Public Class ユニット日誌
         If IsNothing(benForm) OrElse benForm.IsDisposed Then
             Dim unit As String = If(IsNothing(unitListBox.SelectedItem), "", unitListBox.SelectedItem.ToString())
             Dim ad As String = YmdBox.getADStr()
-            Dim wareki As String = YmdBox.getWarekiStr()
             Dim dayStr As String = YmdBox.getDay()
             If unit = "" Then
                 MsgBox("ユニットを選択して下さい。", MsgBoxStyle.Exclamation)
                 Return
             End If
-            benForm = New 便観察(unit, ad, wareki, dayStr, Me.Location.X, Me.Location.Y)
+            benForm = New 便観察(unit, ad, dayStr, Me.Location.X, Me.Location.Y)
             benForm.StartPosition = FormStartPosition.Manual
             benForm.Owner = Me
             benForm.ShowDialog()
